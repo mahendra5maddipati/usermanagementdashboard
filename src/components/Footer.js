9bb7f73f005e947b57a590ba86e2f96afcfee62a
mahendra maddipati <mahendra5maddipati@gmail.com>
2025-01-28 01:45:38 +0530

    Last Modified: 2021-08-17 16:00:00

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <p>Developed by Your Name</p>
+    <p>Developed by Mahendra Maddipati</p>
     <p>Email: <a href="mahendra5maddipati@gmail.com">mahendra5maddipati@gmail.com</a></p>
   </footer>
 );
