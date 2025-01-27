3203a4c5a068591fcb310fc61ea649de14766524
mahendra maddipati <mahendra5maddipati@gmail.com>
2025-01-27 09:45:40 +0530

    Added main Application wrapper in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Dashboard from "./pages/Dashboard";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Management Dashboard</h1>
+      {/* <Dashboard /> */}
     </div>
   );
 }
 
 export default App;
-
